444c1c3180c99bcc01277d54f06aa64fd8afbbfe
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-01-22 19:43:45 -0300

    Se aplicado esse commit, verifica se existe um usuario "logado"

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import Header from '../componentes/Header';
 import audioValid from '../audio/ranking.mp3';
 import audioError from '../audio/ranking_erro.mp3';
+import { getTokenLocalStorage } from '../server';
 
 const music = new Audio(audioValid);
 const erro = new Audio(audioError);
@@ -34,71 +35,72 @@ class Feedback extends Component {
   render() {
     const { history, score, assertions } = this.props;
     return (
-      <div
-        className="
+      getTokenLocalStorage() ? (
+        <div
+          className="
           flex flex-col items-center
           min-h-screen
           bg-ultra-purple"
-      >
-        <Header />
-        <div
-          className="
+        >
+          <Header />
+          <div
+            className="
             flex flex-col justify-items-center justify-center
             max-w-xl min-h
             bg-conf bg-cover
             text-white
             shadow-2xl rounded-3xl
             shadow-gray-700"
-        >
-          <div
-            className="flex flex-col text-center items-center"
           >
-            <h1
-              className="text-5xl p-10 font-bebas"
-              data-testid="feedback-text"
-            >
-              {this.playMusic(assertions)}
-            </h1>
             <div
-              className="
+              className="flex flex-col text-center items-center"
+            >
+              <h1
+                className="text-5xl p-10 font-bebas"
+                data-testid="feedback-text"
+              >
+                {this.playMusic(assertions)}
+              </h1>
+              <div
+                className="
                 flex flex-col justify-center
                 bg-yellow-500
                 w-60 h-60
                 rounded-full"
-            >
-              <span
-                className="mt-12 text-8xl leading-10"
-                data-testid="feedback-total-score"
               >
-                {' '}
-                { score }
-              </span>
-              <span
-                className="font-bebas text-3xl p-5"
-                data-testid="feedback-total-question"
-              >
-                Right awsers:
-                {' '}
-                { assertions }
-              </span>
+                <span
+                  className="mt-12 text-8xl leading-10"
+                  data-testid="feedback-total-score"
+                >
+                  {' '}
+                  { score }
+                </span>
+                <span
+                  className="font-bebas text-3xl p-5"
+                  data-testid="feedback-total-question"
+                >
+                  Right awsers:
+                  {' '}
+                  { assertions }
+                </span>
+              </div>
             </div>
-          </div>
-          <div className="flex m-10">
-            <button
-              className="w-full text-xl mt-5 text-white bg-gradient-to-r from-purple-500
+            <div className="flex m-10">
+              <button
+                className="w-full text-xl mt-5 text-white bg-gradient-to-r from-purple-500
               via-purple-600 to-purple-700 focus:bg-gradient-to-br hover:ring-4
               hover:ring-purple-300 dark:hover:ring-purple-800 shadow-lg
               shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80
               font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-              data-testid="btn-play-again"
-              type="button"
-              onClick={ () => history.push('/') }
-            >
-              Play Again
-            </button>
-            <div />
-            <button
-              className="
+                data-testid="btn-play-again"
+                type="button"
+                onClick={ () => history.push('/') }
+              >
+                Play Again
+              </button>
+              <div />
+              <button
+                className="
                 w-full
                 text-xl mt-5
                 text-black
@@ -107,15 +109,16 @@ class Feedback extends Component {
                 hover:ring-blue-300 dark:hover:ring-blue-800 shadow-lg
                 shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80
                 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-              data-testid="btn-ranking"
-              type="button"
-              onClick={ () => history.push('/ranking') }
-            >
-              Ranking
-            </button>
+                data-testid="btn-ranking"
+                type="button"
+                onClick={ () => history.push('/ranking') }
+              >
+                Ranking
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      ) : (<div>{history.push('/NotFound')}</div>)
     );
   }
 }
--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { getRankingLocalStorage } from '../server';
+import { getRankingLocalStorage, getTokenLocalStorage } from '../server';
 import goldMedal from '../img/medalha_ouro.png';
 import silverMedal from '../img/medalha_prata.png';
 import bronzeMedal from '../img/medalha_bronze.png';
@@ -12,74 +12,78 @@ class Ranking extends Component {
     const NEGATIVEORDER = -1;
 
     return (
-      <div className=" bg-ultra-purple pt-4 min-h-screen">
-        <h1
-          data-testid="ranking-title"
-          className="text-center text-8xl text-white font-bebas"
-        >
-          Ranking
-        </h1>
-        <div>
-          <button
-            data-testid="btn-go-home"
-            type="button"
-            onClick={ () => history.push('/') }
-            className="text-white ml-10 font-comic text-3xl"
+      getTokenLocalStorage() ? (
+        <div className=" bg-ultra-purple pt-4 min-h-screen">
+          <h1
+            data-testid="ranking-title"
+            className="text-center text-8xl text-white font-bebas"
           >
-            &#8592;
-            Login
-          </button>
-        </div>
-        <div className="flex justify-center w-full">
-          <ul
-            className="flex flex-col items-center
+            Ranking
+          </h1>
+          <div>
+            <button
+              data-testid="btn-go-home"
+              type="button"
+              onClick={ () => history.push('/') }
+              className="text-white ml-10 font-comic text-3xl"
+            >
+              &#8592;
+              Login
+            </button>
+          </div>
+          <div className="flex justify-center w-full">
+            <ul
+              className="flex flex-col items-center
             bg-conf bg-cover w-4/6 rounded-3xl py-6 mb-4
             shadow-2xl rounded-3xl shadow-gray-700"
-          >
-            { JSON.parse(getRankingLocalStorage()).sort((a, b) => {
-              if (a.score < b.score) {
-                return 1;
-              }
-              if (a.score > b.score) {
-                return NEGATIVEORDER;
-              }
-              return 0;
-            }).map((obj, key) => (
-              <div
-                key={ key }
-                className="flex mb-4 gap-4 text-xl text-white
+            >
+              { JSON.parse(getRankingLocalStorage()).sort((a, b) => {
+                if (a.score < b.score) {
+                  return 1;
+                }
+                if (a.score > b.score) {
+                  return NEGATIVEORDER;
+                }
+                return 0;
+              }).map((obj, key) => (
+                <div
+                  key={ key }
+                  className="flex mb-4 gap-4 text-xl text-white
                 bg-gradient-to-r from-gray-800 to-gray-900 w-4/5 rounded-full"
-              >
-                <li>
-                  <img
-                    src={ obj.picture }
-                    alt=""
-                    className="rounded-full"
-                  />
+                >
+                  <li>
+                    <img
+                      src={ obj.picture }
+                      alt=""
+                      className="rounded-full"
+                    />
 
-                </li>
-                <div className="flex justify-between items-center w-4/5">
-                  <li
-                    data-testid={ `player-name-${key}` }
-                    className="w-14 text-ellipsis"
-                  >
-                    {obj.nome}
                   </li>
+                  <div className="flex justify-between items-center w-4/5">
+                    <li
+                      data-testid={ `player-name-${key}` }
+                      className="w-14 text-ellipsis"
+                    >
+                      {obj.nome}
+                    </li>
 
-                  {key === 0 && <li><img src={ goldMedal } alt="" className="w-8" /></li>}
+                    {key === 0
+                      && <li><img src={ goldMedal } alt="" className="w-8" /></li>}
 
-                  {key === 1
+                    {key === 1
                     && <li><img src={ silverMedal } alt="" className="w-8" /></li>}
 
-                  {key === 2
+                    {key === 2
                     && <li><img src={ bronzeMedal } alt="" className="w-9" /></li>}
 
-                  <li data-testid={ `player-score-${key}` }>{obj.score}</li>
-                </div>
-              </div>))}
-          </ul>
+                    <li data-testid={ `player-score-${key}` }>{obj.score}</li>
+                  </div>
+                </div>))}
+            </ul>
+          </div>
         </div>
-      </div>
+      ) : (<div>{history.push('/NotFound')}</div>)
+
     );
   }
 }
